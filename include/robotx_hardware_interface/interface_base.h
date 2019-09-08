66fad16ec1d51be79da22d11f3131b7aa3ac9dc5
Masaya Kataoka <ms.kataoka@gmail.com>
2019-09-08 09:19:19 +0900

    add marker for robot status

--- include/robotx_hardware_interface/interface_base.h
+++ include/robotx_hardware_interface/interface_base.h
@@ -4,6 +4,8 @@
 // Headers in ROS
 #include <ros/ros.h>
 #include <usv_control_msgs/AzimuthThrusterCatamaranDriveStamped.h>
+#include <visualization_msgs/MarkerArray.h>
+#include <quaternion_operation/quaternion_operation.h>
 
 // Headers in Boost
 #include <boost/optional.hpp>
@@ -11,6 +13,7 @@
 
 // Headers in STL
 #include <mutex>
+#include <float.h>
 
 class InterfaceBase
 {
@@ -29,6 +32,17 @@ private:
     double output_rate_;
     ros::Subscriber cmd_sub_;
     std::mutex mtx_;
+    ros::Publisher marker_pub_;
+    visualization_msgs::MarkerArray generateMarker();
+    std::string robot_frame_;
+    inline bool isEquals(double a,double b)
+    {
+        if (fabs(a - b) <= DBL_EPSILON * fmax(1, fmax(fabs(a), fabs(b))))
+        {
+            return true;
+        }
+        return false;
+    }
 };
 
 #endif  //ROBOTX_HARDWARE_INTERFACE_INTERFACE_BASE_H_INCLUDED
--- src/interface_base.cpp
+++ src/interface_base.cpp
@@ -4,7 +4,9 @@ InterfaceBase::InterfaceBase(ros::NodeHandle nh,ros::NodeHandle pnh)
 {
     nh_ = nh;
     pnh_ = pnh;
+    pnh_.param<std::string>("robot_frame", robot_frame_, "base_link");
     pnh_.param<double>("output_rate", output_rate_, 30);
+    marker_pub_ = pnh_.advertise<visualization_msgs::MarkerArray>("marker",1);
     cmd_sub_ = nh_.subscribe("/current_command",1,&InterfaceBase::currentCommandCallback,this);
 }
 
@@ -17,7 +19,98 @@ void InterfaceBase::currentCommandCallback(const usv_control_msgs::AzimuthThrust
 {
     mtx_.lock();
     cmd_ = *msg;
+    marker_pub_.publish(generateMarker());
     mtx_.unlock();
+}
+
+visualization_msgs::MarkerArray InterfaceBase::generateMarker()
+{
+    visualization_msgs::MarkerArray marker_msg;
+    double marker_position_x = -2.5;
+    double marker_position_y =  1.5;
+    double marker_position_z =  0.0;
+    double marker_size = 0.2;
+    geometry_msgs::Quaternion quat;
+    quat.x = 0.0;
+    quat.y = 0.0;
+    quat.z = 0.0;
+    quat.w = 1.0;
+    geometry_msgs::Vector3 vec;
+    vec.x = 0.0;
+    vec.y = M_PI/2;
+    vec.z = 0.0;
+    if(cmd_)
+    {
+        if(!isEquals(cmd_->command.left_thrust_cmd,0.0))
+        {
+            visualization_msgs::Marker left_thrust_marker;
+            left_thrust_marker.header.frame_id = robot_frame_;
+            left_thrust_marker.header.stamp = cmd_->header.stamp;
+            left_thrust_marker.ns = "left_thrust";
+            left_thrust_marker.id = 0;
+            left_thrust_marker.type = visualization_msgs::Marker::CYLINDER;
+            left_thrust_marker.action = visualization_msgs::Marker::ADD;
+            left_thrust_marker.pose.position.x = marker_position_x;
+            left_thrust_marker.pose.position.y = marker_position_y;
+            left_thrust_marker.pose.position.z = marker_position_z+cmd_->command.left_thrust_cmd*0.5;
+            left_thrust_marker.pose.orientation = quat;//*quaternion_operation::convertEulerAngleToQuaternion(vec);
+            left_thrust_marker.scale.x = marker_size;
+            left_thrust_marker.scale.y = marker_size;
+            left_thrust_marker.scale.z = -1*cmd_->command.left_thrust_cmd;
+            if(cmd_->command.left_thrust_cmd>0.0)
+            {
+                left_thrust_marker.color.r = 0.3;
+                left_thrust_marker.color.g = 1.0;
+                left_thrust_marker.color.b = 0.3;
+                left_thrust_marker.color.a = 0.8;
+            }
+            else
+            {
+                left_thrust_marker.color.r = 1.0;
+                left_thrust_marker.color.g = 0.3;
+                left_thrust_marker.color.b = 0.3;
+                left_thrust_marker.color.a = 0.8;
+            }
+            left_thrust_marker.frame_locked = true;
+            left_thrust_marker.lifetime = ros::Duration(0.1);
+            marker_msg.markers.push_back(left_thrust_marker);
+        }
+        if(!isEquals(cmd_->command.right_thrust_cmd,0.0))
+        {
+            visualization_msgs::Marker right_thrust_marker;
+            right_thrust_marker.header.frame_id = robot_frame_;
+            right_thrust_marker.header.stamp = cmd_->header.stamp;
+            right_thrust_marker.ns = "right_thrust";
+            right_thrust_marker.id = 0;
+            right_thrust_marker.type = visualization_msgs::Marker::CYLINDER;
+            right_thrust_marker.action = visualization_msgs::Marker::ADD;
+            right_thrust_marker.pose.position.x = marker_position_x;
+            right_thrust_marker.pose.position.y = -marker_position_y;
+            right_thrust_marker.pose.position.z = marker_position_z+cmd_->command.right_thrust_cmd*0.5;
+            right_thrust_marker.pose.orientation = quat;//*quaternion_operation::convertEulerAngleToQuaternion(vec);
+            right_thrust_marker.scale.x = marker_size;
+            right_thrust_marker.scale.y = marker_size;
+            right_thrust_marker.scale.z = -1*cmd_->command.right_thrust_cmd;
+            if(cmd_->command.right_thrust_cmd>0.0)
+            {
+                right_thrust_marker.color.r = 0.3;
+                right_thrust_marker.color.g = 1.0;
+                right_thrust_marker.color.b = 0.3;
+                right_thrust_marker.color.a = 0.8;
+            }
+            else
+            {
+                right_thrust_marker.color.r = 1.0;
+                right_thrust_marker.color.g = 0.3;
+                right_thrust_marker.color.b = 0.3;
+                right_thrust_marker.color.a = 0.8;
+            }
+            right_thrust_marker.frame_locked = true;
+            right_thrust_marker.lifetime = ros::Duration(0.1);
+            marker_msg.markers.push_back(right_thrust_marker);
+        }
+    }
+    return marker_msg;
 }
 
 void InterfaceBase::run()
@@ -33,7 +126,14 @@ void InterfaceBase::sendCommand()
         if(cmd_)
         {
             mtx_.lock();
-            sendCommandToTheTarget(*cmd_);
+            try
+            {
+                sendCommandToTheTarget(*cmd_);
+            }
+            catch(std::exception& e)
+            {
+                ROS_WARN_STREAM(e.what());
+            }
             mtx_.unlock();
         }
         rate.sleep();
